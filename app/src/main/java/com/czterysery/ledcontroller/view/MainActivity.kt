25daf8f66a6988816ba5ff9fc34bc199ec4efbf1
tomek <tmax0135@gmail.com>
2019-12-30 13:31:24 +0100

    Fixed color picker size and button style

--- app/src/main/java/com/czterysery/ledcontroller/view/MainActivity.kt
+++ app/src/main/java/com/czterysery/ledcontroller/view/MainActivity.kt
@@ -20,7 +20,6 @@ class MainActivity : AppCompatActivity(), MainView, ColorObserver {
     private var connected = false
 
 
-    //TODO Set text apperance, style the button, add visibility states
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
