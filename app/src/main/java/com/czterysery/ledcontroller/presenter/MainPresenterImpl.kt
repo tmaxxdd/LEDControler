1dade698a4140df4492b0b4bc89c132a379096ee
tkadziolka <tmax0135@gmail.com>
2020-01-11 14:38:45 +0100

    Added sending connection message

--- app/src/main/java/com/czterysery/ledcontroller/presenter/MainPresenterImpl.kt
+++ app/src/main/java/com/czterysery/ledcontroller/presenter/MainPresenterImpl.kt
@@ -118,7 +118,10 @@ class MainPresenterImpl(
 
     private fun onConnectionStateChanged(state: ConnectionState) {
         when (state) {
-            is Connected -> tryToGetConfiguration(state.device)
+            is Connected -> {
+                sendConnectionMessage(connected = true)
+                tryToGetConfiguration(state.device)
+            }
             Disconnected -> view?.showDisconnected()
             is Error -> view?.showError(state.message)
         }
