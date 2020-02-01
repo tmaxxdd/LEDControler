e4516ecf05c3062c0561fd82100d71641e035a16
tomek <tmax0135@gmail.com>
2020-02-01 10:17:21 +0100

    Created value mappers

--- app/src/main/java/com/czterysery/ledcontroller/data/mapper/ConfigurationMapper.kt
+++ app/src/main/java/com/czterysery/ledcontroller/data/mapper/ConfigurationMapper.kt
@@ -1,9 +1,11 @@
 package com.czterysery.ledcontroller.data.mapper
 
-import android.util.JsonReader
+import android.graphics.Color
 import com.czterysery.ledcontroller.data.model.Configuration
-import com.squareup.moshi.Moshi
-import org.json.JSONObject
+import com.czterysery.ledcontroller.exceptions.InvalidBrightnessValueException
+import com.czterysery.ledcontroller.exceptions.InvalidColorValueException
+import com.czterysery.ledcontroller.exceptions.InvalidConfigurationMessageException
+import java.lang.Exception
 
 // CONF=clr:#2233ff,brig:231,illu:1\n
 const val colorLength = 7 // #2233ff
@@ -12,41 +14,44 @@ const val brightnessPrefix = "brig:"
 const val illuminationPrefix = "illu:"
 val brightnessRange = 0..255
 
-// TODO Read about moshi and implement https://github.com/square/moshi
-// TODO Write unit tests
+// TODO Write unit test to this class
 class ConfigurationMapper {
-    fun map(message: String): Configuration =
+    fun map(message: String) =
+        Configuration(
+            getColor(message),
+            getBrightness(message),
+            getIllumination()
+        )
 
+    private fun getColor(message: String): Int {
+        if (message.contains("(clr:#).{6}".toRegex())) {
+            val start = message.indexOf(colorPrefix) + 4 // end of 'clr:'
+            return try {
+                Color.parseColor(message.substring(start, start + (colorLength)))
+            } catch (e: Exception) {
+                throw InvalidColorValueException()
+            }
+        } else {
+            throw InvalidConfigurationMessageException()
+        }
+    }
 
-//    private fun getColor(message: String): Int {
-//        if (message.contains("(clr:#).{6}".toRegex())) {
-//            val start = message.indexOf(colorPrefix) + 4 // end of 'clr:'
-//            return try {
-//                Color.parseColor(message.substring(start, start + (colorLength)))
-//            } catch (e: Exception) {
-//                throw InvalidColorValueException()
-//            }
-//        } else {
-//            throw InvalidConfigurationMessageException()
-//        }
-//    }
-//
-//    private fun getBrightness(message: String): Int {
-//        if (message.contains("(brig:)".toRegex())) {
-//            val start = message.indexOf(brightnessPrefix) + 5 // end of 'brig:'
-//            while (message[start] is Number)
-//            return if (color in brightnessRange)
-//                color
-//            else
-//                throw InvalidBrightnessValueException()
-//        } else {
-//            throw InvalidConfigurationMessageException()
-//        }
-//    }
-//
-//    private fun getIllumination() {
-//
-//    }
+    private fun getBrightness(message: String): Int {
+        if (message.contains("(brig:)".toRegex())) {
+            val start = message.indexOf(brightnessPrefix) + 5 // end of 'brig:'
+            while (message[start] is Number)
+            return if (color in brightnessRange)
+                color
+            else
+                throw InvalidBrightnessValueException()
+        } else {
+            throw InvalidConfigurationMessageException()
+        }
+    }
+
+    private fun getIllumination() {
+
+    }
 
 
 }
